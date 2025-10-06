61ee97174527fc370eb9eb7bdff4bcf237973a01
jagadesh-31 <jjagadesh980@gmail.com>
2025-10-06 18:47:21 +0530

    error fixed

--- server/controllers/otp.js
+++ server/controllers/otp.js
@@ -1,6 +1,6 @@
 const otpModel = require('../models/otp.js')
 const userModel = require('../models/user.js')
-const { sendOtp } = require('../sendMail.js')
+// const { sendOtp } = require('../sendMail.js')
 
 let createOtp = async (req, res) => {
   console.log('entered')
@@ -12,25 +12,29 @@ let createOtp = async (req, res) => {
   try {
     let userRes = await userModel.findOne({ email: email });
     console.log(userRes)
-    if (userRes) {
-      console.log('found')
-      let Res = await otpModel.findOne({ email: email });
-      if(Res){
-       return res.status(200).json({message:'Already Sended wait 2 min'})
-      } else{
-      let sendOtpRes = await otpModel.insertOne({ email: email, otp: otp });
-      console.log(sendOtpRes)
-      if (sendOtpRes) {
-        sendOtp(email, otp);
-        console.log('error')
-        res.status(200).json({message:'success'})
-        console.log('successfully added')
-      }
-          else { console.log('failed in adding') }}
-    } else {
-      res.status(404).json({ message: 'email not found' })
-      console.log('email not found')
-    }
+
+   return  res.status(402).json({ message: "issue with nodemailer" });
+
+
+    // if (userRes) {
+    //   console.log('found')
+    //   let Res = await otpModel.findOne({ email: email });
+    //   if(Res){
+    //    return res.status(200).json({message:'Already Sended wait 2 min'})
+    //   } else{
+    //   let sendOtpRes = await otpModel.insertOne({ email: email, otp: otp });
+    //   console.log(sendOtpRes)
+    //   if (sendOtpRes) {
+    //     // sendOtp(email, otp);
+    //     console.log('error')
+    //     res.status(200).json({message:'success'})
+    //     console.log('successfully added')
+    //   }
+    //       else { console.log('failed in adding') }}
+    // } else {
+    //   res.status(404).json({ message: 'email not found' })
+    //   console.log('email not found')
+    // }
   } catch (err) {
     res.status(402).json({ message: err.response })
   }
