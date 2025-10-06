69e791726a352a5451a092deccfb64515e9fbb96
jagadesh310 <gamerdevil0346@gmail.com>
2025-10-07 00:43:56 +0530

    Update pdfGenerator.js

--- server/pdfGenerator.js
+++ server/pdfGenerator.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const fs = require('fs');
 const Handlebars = require('handlebars');
 const puppeteer = require('puppeteer');
@@ -7,30 +8,31 @@ Handlebars.registerHelper('eq', function (a, b) {
   return a === b;
 });
 
-
 async function generatePdf(data) {
   const plainData = JSON.parse(JSON.stringify(data));
   
-  const qr = await qrCode.toDataURL(`http://localhost:5000/ticket/track?id=${ data._id }`);
+  const qr = await qrCode.toDataURL(`${process.env.server_url}/ticket/track?id=${ data._id }`);
 
   plainData.qrcode = qr;
-   
-  const templateStr = fs.readFileSync('templates/invoice.hbs', 'utf8');
+
+  const templatePath = path.join(__dirname, 'templates', 'invoice.hbs');
+  const templateStr = fs.readFileSync(templatePath, 'utf8');
 
   const compiledTemplate = Handlebars.compile(templateStr);
   const html = compiledTemplate(plainData);
 
-const browser = await puppeteer.launch({
-  headless: true, 
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
-});
+  const browser = await puppeteer.launch({
+    headless: true, 
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
   const page = await browser.newPage();
 
   await page.setContent(html, { waitUntil: 'load' });
 
   const pdfBuffer = await page.pdf({
-  printBackground: true,
-  preferCSSPageSize: true 
+    printBackground: true,
+    preferCSSPageSize: true 
   });
 
   await browser.close();
